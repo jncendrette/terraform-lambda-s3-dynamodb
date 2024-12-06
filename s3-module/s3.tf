36086437f662a70cddd5d8aaf9e97694cdd48b04
José <jnc.vip@gmail.com>
2024-12-06 19:10:51 -0300

    :but: fix: Line 3 add for delete bucket not-empty

--- s3-module/s3.tf
+++ s3-module/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "default" {
-  bucket = join("-", [var.bucket_name, data.aws_caller_identity.user.account_id, var.env])
+  bucket        = join("-", [var.bucket_name, data.aws_caller_identity.user.account_id, var.env])
+  force_destroy = true
   tags = {
     Name        = var.bucket_name
     Environment = var.env
